706e3cc59038d46b7a48956f7e858e0260370559
jarrodskahill <jarrodskahill@gmail.com>
2020-07-31 20:39:20 -0700

    fix

--- knexfile.js
+++ knexfile.js
@@ -27,10 +27,10 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "/db/migrations",
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: "/db/seeds",
+      directory: "./db/seeds",
     },
   },
 };
