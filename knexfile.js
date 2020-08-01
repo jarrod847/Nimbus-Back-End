709c38fc65d8cae6559ffd9373f76906bc51b60d
jarrodskahill <jarrodskahill@gmail.com>
2020-07-31 20:25:07 -0700

    more changes to knex

--- knexfile.js
+++ knexfile.js
@@ -5,32 +5,32 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "./database/migrations",
+      directory: "./db/migrations",
       tableName: "dbmigrations",
     },
-    seeds: { directory: "./database/seeds" },
+    seeds: { directory: "./db/seeds" },
   },
   testing: {
     client: "pg",
     connection: {
-      filename: "./database/test.db3",
+      filename: "./db/test.db3",
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./database/migrations",
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: "./database/seeds",
+      directory: "./db/seeds",
     },
   },
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "/database/migrations",
+      directory: "/db/migrations",
     },
     seeds: {
-      directory: "/database/seeds",
+      directory: "/db/seeds",
     },
   },
 };
