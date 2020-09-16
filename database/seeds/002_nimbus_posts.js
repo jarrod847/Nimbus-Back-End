bb2188a1ff85871e509a6d847e83a4c00db199c7
jarrodskahill <jarrodskahill@gmail.com>
2020-09-16 01:12:17 -0700

    connected tables for user_id on posts

--- database/seeds/002_nimbus_posts.js
+++ database/seeds/002_nimbus_posts.js
@@ -5,15 +5,23 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("posts").insert([
-        { id: 1, content: "Whats good fools", likes: 50, reposts: 5, img: "" },
+        {
+          id: 1,
+          content: "Whats good fools",
+          likes: 50,
+          reposts: 5,
+          img: "",
+          user_id: 1,
+        },
         {
           id: 2,
           content: "second one for the count",
           likes: 5,
           reposts: 1,
           img: "",
+          user_id: 1,
         },
-        { id: 3, content: "We out here tho", likes: 10, img: "" },
+        { id: 3, content: "We out here tho", likes: 10, img: "", user_id: 1 },
       ]);
     });
 };
