33d5ac8fd7313848c8832b4a19b788f1a5d3ba34
jarrodskahill <jarrodskahill@gmail.com>
2020-12-12 18:53:34 -0600

    started creating some functions for comments

--- comments/comment-model.js
+++ comments/comment-model.js
@@ -5,5 +5,18 @@ module.exports = {
   addComment,
   findCommentsByUser,
   findCommentsForPost,
+  findCommentById,
   deleteComment,
 };
+
+function allComments() {
+  return db("postComments");
+}
+
+function findCommentById(id) {
+  return db("postComments").where({ id }).first();
+}
+
+function deleteComment(id) {
+  return findCommentById(id).del(id);
+}
